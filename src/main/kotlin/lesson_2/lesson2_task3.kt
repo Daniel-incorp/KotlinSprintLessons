a624f391343acb86f5abadf290d0c7ab2b6c3814
Daniel <daniel.work9@mail.ru>
2025-07-12 14:53:44 +0300

    KS-2-3 task3 work with time_self_refactored

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,11 +1,12 @@
 package lesson_2
+const val ROUND = 60
 
 fun main() {
     val routeTime = 457
     val routeHours = 9
     val routeMinutes = 39
-    val arrivedMinutes = (routeTime + routeHours * 60 + routeMinutes) % 60
-    val arrivedHours =  (routeTime + routeHours * 60 + routeMinutes) / 60
+    val arrivedMinutes = (routeTime + routeHours * ROUND + routeMinutes) % ROUND
+    val arrivedHours =  (routeTime + routeHours * ROUND + routeMinutes) / ROUND
 
 /*Ещё один вариант решения, более геморойный, но почему то он мне первый пришёл в голову...
 где мы отдельно высчитываем часы в routeTime прибавляем их к routeHours, а оставшиеся минуты
