d6b18db0f4540e2dade0193d252605f331ee0375
Daniel <daniel.work9@mail.ru>
2025-07-12 12:28:15 +0300

    KS-1-5 task5 work with seconds_refactored

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,17 @@
 package lesson_1
+const val ROUND = 60
 
 fun main() {
     val seconds = 6480
-    val hoursInSpace = seconds / (60*60)
-    val minutesInSpace = seconds / 60 - hoursInSpace * 60
+    val hoursInSpace = seconds / (ROUND*ROUND)
+    val minutesInSpace = seconds / ROUND % ROUND
 //Всегда будет 0, т.к. всё делится нацело, если было бы, например, число 6481, то ответ был бы 1
-    val secondsInSpace = seconds - hoursInSpace * 60 * 60 - minutesInSpace * 60
-/*Можно сделать обработку до 24ч и динамически добавлять/убирать незначащий 0, но, как я предполагаю,
-подобное будет далее, поэтому просто вставлю костыли:)*/
+    val secondsInSpace = seconds % ROUND
+//Оказывается всё куда проще, а я изначально всю обработку незначащих нулей зафигачил в 3 условия when...
 
-    println("\n0$hoursInSpace:$minutesInSpace:0$secondsInSpace")
+    println(String.format("\n%02d:%02d:%02d",
+        hoursInSpace,
+        minutesInSpace,
+        secondsInSpace
+    ))
 }
