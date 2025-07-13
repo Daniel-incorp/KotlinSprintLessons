8dd6a01316c2de31a0c363dad86528d3958197d7
Daniel <daniel.work9@mail.ru>
2025-07-13 14:21:30 +0300

    KS-2-3 task3 work with time

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,22 +1,14 @@
 package lesson_2
-const val ROUND = 60
+
+
+const val MINUTES_IN_HOUR = 60
 
 fun main() {
     val routeTime = 457
     val routeHours = 9
     val routeMinutes = 39
-    val arrivedMinutes = (routeTime + routeHours * ROUND + routeMinutes) % ROUND
-    val arrivedHours =  (routeTime + routeHours * ROUND + routeMinutes) / ROUND
+    val arrivedMinutes = (routeTime % MINUTES_IN_HOUR + routeMinutes) % MINUTES_IN_HOUR
+    val arrivedHours = routeTime / MINUTES_IN_HOUR + routeHours + (routeTime % MINUTES_IN_HOUR + routeMinutes) / MINUTES_IN_HOUR
 
-/*Ещё один вариант решения, более геморойный, но почему то он мне первый пришёл в голову...
-где мы отдельно высчитываем часы в routeTime прибавляем их к routeHours, а оставшиеся минуты
-прибавляем к routeMinutes, при чём нужно учитывать, если arrivedMinutes > 60 то в arrivedHours должно быть + 1
-Результаты вычислений одинаковые - 17:16
-
-    val arrivedMinutes = (routeTime % 60 + routeMinutes) % 60
-    val arrivedHours = routeTime / 60 + routeHours + (routeTime % 60 + routeMinutes) / 60
-
-*/
-
-    println("\n$arrivedHours:$arrivedMinutes")
+    println("Время прибытия поезда: %02d:%02d".format(arrivedHours, arrivedMinutes))
 }
