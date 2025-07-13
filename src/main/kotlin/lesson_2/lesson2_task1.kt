171d8224a002c1e2e10916b7b1790da2140ed278
Daniel <daniel.work9@mail.ru>
2025-07-13 11:50:32 +0300

    KS-2-1 task1 work with school software

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
 fun main() {
-    val firstStudentMark = 3.00f
-    val secondStudentMark = 4.00f
-    val thirdStudentMark = 3.00f
-    val fourthStudentMark = 5.00f
+    val firstStudentMark = 3f
+    val secondStudentMark = 4f
+    val thirdStudentMark = 3f
+    val fourthStudentMark = 5f
     val numberOfStudents = 4
     val averageRating = (firstStudentMark + secondStudentMark + thirdStudentMark + fourthStudentMark) / numberOfStudents
 
 
-    println(String.format("\nСредний балл студентов: %.2f", averageRating))
+    println(String.format("%nСредний балл студентов: %.2f", averageRating))
 }
