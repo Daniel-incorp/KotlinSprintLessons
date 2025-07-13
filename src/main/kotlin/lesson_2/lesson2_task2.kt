45d510f1a800ecdc077ded5a197499f8a4f2d93f
Daniel <daniel.work9@mail.ru>
2025-07-13 18:25:29 +0300

    KS-2-2 task2 work with salary

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -3,11 +3,12 @@ package lesson_2
 fun main() {
     val employees = 50
     val interns = 30
-    val salaryForEmployee = 50000
+    val salaryForEmployee = 30000
     val salaryForIntern = 20000
     val salaryForAllEmployees  = employees * salaryForEmployee
-    val salaryForAll = employees * salaryForEmployee + interns * salaryForIntern
-    val averageSalary = (employees * salaryForEmployee + interns * salaryForIntern)  / (employees + interns)
+    val salaryForAllInterns = interns * salaryForIntern
+    val salaryForAll = salaryForAllEmployees + salaryForAllInterns
+    val averageSalary = salaryForAll  / (employees + interns)
 
     println(
         "Выплата постоянным сотрудникам: $salaryForAllEmployees рублей\n" +
