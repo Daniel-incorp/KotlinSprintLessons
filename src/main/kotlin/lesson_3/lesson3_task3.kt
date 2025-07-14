5421e0e7e0534ee38f9f660f88e6c4e02f27a0c9
Daniel <daniel.work9@mail.ru>
2025-07-14 19:00:19 +0300

    KS-3-3 task3 work with multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,17 @@
+package lesson_3
+
+fun main() {
+    val numForMultiply = 6
+
+    println("""
+        $numForMultiply * 1 = ${numForMultiply * 1}
+        $numForMultiply * 2 = ${numForMultiply * 2}
+        $numForMultiply * 3 = ${numForMultiply * 3}
+        $numForMultiply * 4 = ${numForMultiply * 4}
+        $numForMultiply * 5 = ${numForMultiply * 5}
+        $numForMultiply * 6 = ${numForMultiply * 6}
+        $numForMultiply * 7 = ${numForMultiply * 7}
+        $numForMultiply * 8 = ${numForMultiply * 8}
+        $numForMultiply * 9 = ${numForMultiply * 9}
+    """.trimIndent())
+}
