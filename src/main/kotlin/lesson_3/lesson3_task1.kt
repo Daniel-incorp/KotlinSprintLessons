f5164fa5c6da74797b262b7ddd201a5ef6e46f6f
Daniel <daniel.work9@mail.ru>
2025-07-14 16:35:50 +0300

    KS-3-1 task1 work with strings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    var userHello = "Have a good day"
+    var userName = "Dan"
+
+    println("$userHello, $userName!")
+
+    userHello = "Have a good night"
+    userName = "Kodi"
+
+    println("$userHello, $userName!")
+}
