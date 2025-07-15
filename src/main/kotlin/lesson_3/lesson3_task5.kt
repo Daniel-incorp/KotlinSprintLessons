b7bd7a013a3fc2c2ce6e732ce70dae848f0230dd
Daniel <daniel.work9@mail.ru>
2025-07-15 14:29:25 +0300

    KS-3-5 task4 work with chess parsing

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,12 +2,7 @@ package lesson_3
 
 fun main() {
     val string = "D2-D4;0"
-//  Как будто так понятнее, без непонятных чисел, но чуть более загружено
-//  val startMove_ = string.substringAfter('-').substringBefore(';')
-//  val endMove_ = string.substringBefore('-')
-    val startMove = string.substring(0..1)
-    val endMove = string.substring(3..4)
-    val counterMove = string.substringAfter(';')
+    val (startMove, endMove, counterMove) = string.split('-',';')
 
     println("Начало хода: $startMove\nКонец хода: $endMove\nСчётчик ходов: $counterMove")
 }
