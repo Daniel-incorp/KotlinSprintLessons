53ffc2a3ced92f2ca1d1fd129be43659be467229
Daniel <daniel.work9@mail.ru>
2025-07-15 19:21:38 +0300

    KS-4-4 task4 work with pump

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,17 +1,13 @@
 package lesson_4
 
 fun main() {
-// Интересно, есть ли смысл здесь вынести pumpFor_ переменные в константы или нет?
-    val pumpForHand = false
-    val pumpForFeet = false
-    val pumpForBack = false
-    val pumpForPress = false
     val dayCounter = 5
+    val isEven = dayCounter % 2 == 0
 
     println(
-        "Упражнения для рук: ${!pumpForHand && dayCounter % 2 == 1}\n" +
-        "Упражнения для ног: ${!pumpForFeet && dayCounter % 2 == 0}\n" +
-        "Упражнения для спины: ${!pumpForBack && dayCounter % 2 == 0}\n" +
-        "Упражнения для пресса: ${!pumpForPress && dayCounter % 2 == 1}"
+        "Упражнения для рук: ${!isEven}\n" +
+        "Упражнения для ног: $isEven\n" +
+        "Упражнения для спины: $isEven\n" +
+        "Упражнения для пресса: ${!isEven}"
     )
 }
