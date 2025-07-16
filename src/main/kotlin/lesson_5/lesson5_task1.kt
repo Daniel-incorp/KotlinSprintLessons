683ce2acfbba77d97f66cbe4badaa5f167d385a9
Daniel <daniel.work9@mail.ru>
2025-07-16 16:03:59 +0300

    KS-5-1 task5 work with captcha

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,13 +1,13 @@
 package lesson_5
 
 fun main() {
-    print("Докажите, что вы не робот: 2 + 2 = ")
+    val captchaRandomNumber1 = (0..10).random()
+    val captchaRandomNumber2 = (0..10).random()
+
+    print("Докажите, что вы не робот: $captchaRandomNumber1 + $captchaRandomNumber2 = ")
 
     val userInput = readln().toInt()
-    val result = when(userInput) {
-        4 -> "Добро пожаловать!"
-        else -> "Доступ запрещён."
-    }
+    val captchaResult = if (userInput == captchaRandomNumber1 + captchaRandomNumber2) "Добро пожаловать!" else "Доступ запрещён."
 
-    println(result)
+    println(captchaResult)
 }
