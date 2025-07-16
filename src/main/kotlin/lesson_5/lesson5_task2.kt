509908204742c974c56d8dacbe9b72c603fdd0df
Daniel <daniel.work9@mail.ru>
2025-07-16 16:13:37 +0300

    KS-5-2 task2 work with user's year born

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,12 +1,14 @@
 package lesson_5
 
+import java.time.LocalDate
+
 const val AGE_OF_MAJORITY = 18
 
 fun main() {
     print("Введите год вашего рождения: ")
 
     val userYearOfBorn = readln().toInt()
-    val yearOnToday = java.util.Date().toString().substringAfterLast(" ").toInt()
+    val yearOnToday = LocalDate.now().year
     val userAge = yearOnToday - userYearOfBorn
 
     if (userAge >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
