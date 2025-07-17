5bfaeef53bde1166304429c103fc45969f434f09
Daniel <daniel.work9@mail.ru>
2025-07-17 20:43:41 +0300

    KS-5-3 task3 work with lucky numbers

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -4,10 +4,7 @@ const val MAX_LUCKY_NUMBER = 42
 
 fun main() {
     val firstLuckyNumber = (0..MAX_LUCKY_NUMBER).random()
-//    println(firstLuckyNumber)
     val secondLuckyNumber = (0..MAX_LUCKY_NUMBER).random()
-//    println(secondLuckyNumber)
-
 
     print("Введите первое число от 0 до 42: ")
     val firstUserInput = readln().toInt()
@@ -30,4 +27,5 @@ fun main() {
         ) println("Вы выиграли утешительный приз!")
         else println("Неудача!")
     } else println("Пожалуйста введи числа в диапазоне от 0 до 42")
+    println("Загаданные числа $firstLuckyNumber и $secondLuckyNumber",)
 }
