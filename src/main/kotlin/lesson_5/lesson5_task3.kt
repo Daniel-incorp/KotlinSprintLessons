ecb0918ade342f00f9d50ef6ace337a348806f7a
Daniel <daniel.work9@mail.ru>
2025-07-17 12:03:40 +0300

    KS-5-3 task3 work with lucky numbers

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,18 +1,33 @@
 package lesson_5
 
-const val FIRST_LUCKY_NUMBER = 27
-const val SECOND_LUCKY_NUMBER = 35
+const val MAX_LUCKY_NUMBER = 42
 
 fun main() {
+    val firstLuckyNumber = (0..MAX_LUCKY_NUMBER).random()
+//    println(firstLuckyNumber)
+    val secondLuckyNumber = (0..MAX_LUCKY_NUMBER).random()
+//    println(secondLuckyNumber)
+
+
     print("Введите первое число от 0 до 42: ")
     val firstUserInput = readln().toInt()
 
     print("Введите второе число от 0 до 42: ")
     val secondUserInput = readln().toInt()
 
-    if (firstUserInput in 0..42 && secondUserInput in 0..42) {
-        if(firstUserInput == FIRST_LUCKY_NUMBER && secondUserInput == SECOND_LUCKY_NUMBER) println("Поздравляем! Вы выиграли главный приз!")
-        else if (firstUserInput == FIRST_LUCKY_NUMBER || secondUserInput == SECOND_LUCKY_NUMBER) println("Вы выиграли утешительный приз!")
+    if (firstUserInput in 0..MAX_LUCKY_NUMBER && secondUserInput in 0..MAX_LUCKY_NUMBER) {
+        if (
+            (firstUserInput == firstLuckyNumber ||
+                    firstUserInput == secondLuckyNumber) &&
+            (secondUserInput == firstLuckyNumber ||
+                    secondUserInput == secondLuckyNumber)
+        ) println("Поздравляем! Вы выиграли главный приз!")
+        else if (
+            (firstUserInput == firstLuckyNumber ||
+                    firstUserInput == secondLuckyNumber) ||
+            (secondUserInput == firstLuckyNumber ||
+                    secondUserInput == secondLuckyNumber)
+        ) println("Вы выиграли утешительный приз!")
         else println("Неудача!")
     } else println("Пожалуйста введи числа в диапазоне от 0 до 42")
 }
