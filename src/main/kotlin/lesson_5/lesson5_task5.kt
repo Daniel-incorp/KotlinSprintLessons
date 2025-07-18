8acbf98ea9cb61467e3d2222bbc6dd22ecb098fd
Daniel <daniel.work9@mail.ru>
2025-07-18 14:59:52 +0300

    KS-5-5 task5 work with random lucky numbers 2.0

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -7,28 +7,24 @@ const val TWO_NUMBERS_WIN = 2
 const val ONE_NUMBER_WINS = 1
 
 fun main() {
-    var userInput: Int
+    val luckyListNumbers = List(THREE_NUMBERS_WIN) { (MIN_LUCKY_NUMBER..MAX_LUCKY_NUMBER).random() }
     val userListNumbers = mutableListOf<Int>()
-    val luckyListNumbers = List(3) { (MIN_LUCKY_NUMBER..MAX_LUCKY_NUMBER).random() }
 
-    repeat(3) {
+    repeat(THREE_NUMBERS_WIN) {
         print("Введите число от 0 до 42: ")
-        userInput = readln().toInt()
-        userListNumbers.add(userInput)
+        val userInput = readln().toInt()
+
+        if (userInput in MIN_LUCKY_NUMBER..MAX_LUCKY_NUMBER)
+            userListNumbers.add(userInput)
+        else return println("Пожалуйста вводите числа от 0 до 42")
     }
 
-    val uniqueListGuessedNumbers = luckyListNumbers.intersect(userListNumbers)
-
-    for (i in userListNumbers.indices) {
-        if (userListNumbers[i] !in MIN_LUCKY_NUMBER..MAX_LUCKY_NUMBER)
-            return println("Пожалуйста вводите числа от 0 до 42")
-    }
-
-    when (uniqueListGuessedNumbers.size) {
+    val matchedNumber = luckyListNumbers.intersect(userListNumbers)
+    when (matchedNumber.size) {
         THREE_NUMBERS_WIN -> println("Поздравляем, вы угадали все три числа и выиграли джекпот")
         TWO_NUMBERS_WIN -> println("Поздравляем, вы угадали два числа из трёх и выиграли крупный приз")
         ONE_NUMBER_WINS -> println("Поздравляем, вы угадали одно число из трёх и выиграли утешительный приз")
         else -> println("К сожалению вы не угадали ни одного числа")
     }
-    println(luckyListNumbers)
+    println("Счастливые числа: $luckyListNumbers")
 }
