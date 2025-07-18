7357d5fc41b84b41c16831b72379d6f79d97b41f
Daniel <daniel.work9@mail.ru>
2025-07-18 10:28:37 +0300

    KS-5-6 task6 work with body mass index

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -6,22 +6,26 @@ const val OVER_NORMAL_BODY_WEIGHT = 30
 
 fun main() {
     print("Введите ваш вес(в кг): ")
-    val userWeightInput = readln().toDouble()
+    val userWeightInput = readln().toFloat()
 
     print("Введите ваш рост(в см): ")
     val userGrowthInput = readln().toFloat() / 100
 
     val bodyMassIndex = userWeightInput / (userGrowthInput * userGrowthInput)
 
-    if (bodyMassIndex < NOT_NORMAL_BODY_WEIGHT)
-        println("Недостаточная масса тела: %.02f".format(bodyMassIndex))
-    else if (
-        bodyMassIndex >= NOT_NORMAL_BODY_WEIGHT &&
-        bodyMassIndex < NORMAL_BODY_WEIGHT
-    ) println("Нормальная масса тела: %.02f".format(bodyMassIndex))
-    else if (
-        bodyMassIndex >= NORMAL_BODY_WEIGHT &&
-        bodyMassIndex < OVER_NORMAL_BODY_WEIGHT
-    ) println("Избыточная масса тела: %.02f".format(bodyMassIndex))
-    else println("Ожирение: %.02f".format(bodyMassIndex))
+    when {
+        bodyMassIndex < NOT_NORMAL_BODY_WEIGHT ->
+            println("Недостаточная масса тела: %.02f".format(bodyMassIndex))
+
+        bodyMassIndex >= NOT_NORMAL_BODY_WEIGHT && bodyMassIndex < NORMAL_BODY_WEIGHT ->
+            println("Нормальная масса тела: %.02f".format(bodyMassIndex))
+
+        bodyMassIndex >= NORMAL_BODY_WEIGHT && bodyMassIndex < OVER_NORMAL_BODY_WEIGHT ->
+            println("Избыточная масса тела: %.02f".format(bodyMassIndex))
+
+        else -> println("Ожирение: %.02f".format(bodyMassIndex))
+    }
 }
+
+
+
